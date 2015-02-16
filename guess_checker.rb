6d102d5e82c109773433c0147af0056287f3f9bf
Madelyn Kasula <madelynkasula@gmail.com>
2015-02-16 14:03:36 -0600

    Added white/red pegs for correct guesses

--- guess_checker.rb
+++ guess_checker.rb
@@ -5,22 +5,18 @@ class GuessChecker
     attr_accessor :guess
     
     def initialize(guess)
-        @guess = guess
-        check_computer_guess(@guess)
-    end
-
-    def check_computer_guess(guess)
+        @pegs = []
         guess.each { |color| color_check(color) }
+        p @pegs
     end
 
     def color_check(color)
         print "Is #{color} in your code? (Y/N): "
         color_confirmation = gets.chomp
         if color_confirmation == "Y" || color_confirmation == "y"
-            true
             position_check
         else
-            false
+            @pegs << 'none'
             puts "-------------"
         end
     end
@@ -28,11 +24,11 @@ class GuessChecker
     def position_check
         print "Is it in the correct position? (Y/N): "
         position_confirmation = gets.chomp
-        puts "-------------"
         if position_confirmation == "Y" || position_confirmation == "y"
-            true
+            @pegs << 'red'
         else
-            false
+            @pegs << 'white'
         end
+        puts "-------------"
     end
 end
